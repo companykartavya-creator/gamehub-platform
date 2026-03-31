b9ee689bef89a9d4e06c9e4ddf3e63c1367cda74
companykartavya-creator <company.kartavya@gmail.com>
2026-03-31 18:23:03 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,76 +1,52 @@
-// 🔥 IMPORTS (Firebase CDN)
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
-import { getAuth, signInWithPopup, GoogleAuthProvider } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
-import { getFirestore, doc, setDoc, getDocs, collection } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
-
-<script type="module">
-  // Import the functions you need from the SDKs you need
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.11.0/firebase-app.js";
-  import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.11.0/firebase-analytics.js";
-  // TODO: Add SDKs for Firebase products that you want to use
-  // https://firebase.google.com/docs/web/setup#available-libraries
-
-  // Your web app's Firebase configuration
-  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-  const firebaseConfig = {
-    apiKey: "AIzaSyDAc_qZ24aJyeKmt4O0APmmKYaN8Ukf5xQ",
-    authDomain: "gamehub-29519.firebaseapp.com",
-    projectId: "gamehub-29519",
-    storageBucket: "gamehub-29519.firebasestorage.app",
-    messagingSenderId: "778815045155",
-    appId: "1:778815045155:web:5f05b137874eb0c4d77b96",
-    measurementId: "G-MLT78GCJ3Y"
-  };
-
-  // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
-  const analytics = getAnalytics(app);
-</script>
-
-// INIT
-const app = initializeApp(firebaseConfig);
-const auth = getAuth();
-const provider = new GoogleAuthProvider();
-const db = getFirestore();
-
-let currentUser = "Guest";
-
-// LOGIN
-window.login = function () {
-  signInWithPopup(auth, provider).then(result => {
-    currentUser = result.user.displayName;
-    document.getElementById("user").innerText = "👋 " + currentUser;
-  });
-};
-
-// NAVIGATION
-window.playGame = function (game) {
+// NAVIGATION (buttons fix)
+function playGame(game) {
   window.location.href = "games/" + game;
-};
+}
 
-window.randomGame = function () {
+function randomGame() {
   const games = ["flappy.html", "reaction.html", "click.html"];
   let g = games[Math.floor(Math.random() * games.length)];
   window.location.href = "games/" + g;
+}
+
+// LOGIN (TEMP FIX VERSION)
+function login() {
+  let name = prompt("Enter your name:");
+  if (name) {
+    localStorage.setItem("user", name);
+    document.getElementById("user").innerText = "👋 " + name;
+  }
+}
+
+// LOAD USER
+window.onload = function () {
+  let user = localStorage.getItem("user");
+  if (user) {
+    document.getElementById("user").innerText = "👋 " + user;
+  }
 };
 
-// SAVE SCORE
-window.saveScoreGlobal = async function (score) {
-  await setDoc(doc(db, "scores", currentUser), {
-    name: currentUser,
-    score: score
+// GLOBAL LEADERBOARD (LOCAL VERSION)
+function saveScoreGlobal(score) {
+  let user = localStorage.getItem("user") || "Guest";
+
+  let scores = JSON.parse(localStorage.getItem("scores")) || [];
+
+  scores.push({ name: user, score: score });
+
+  localStorage.setItem("scores", JSON.stringify(scores));
+}
+
+function loadLeaderboard() {
+  let scores = JSON.parse(localStorage.getItem("scores")) || [];
+
+  scores.sort((a, b) => b.score - a.score);
+
+  let html = "";
+
+  scores.slice(0, 10).forEach(s => {
+    html += s.name + " - " + s.score + "<br>";
   });
-};
 
-// LOAD LEADERBOARD
-window.loadLeaderboard = async function () {
-  const querySnapshot = await getDocs(collection(db, "scores"));
-  let list = "";
-
-  querySnapshot.forEach(doc => {
-    let data = doc.data();
-    list += data.name + " - " + data.score + "<br>";
-  });
-
-  document.getElementById("leaderboard").innerHTML = list;
-};
+  document.getElementById("leaderboard").innerHTML = html;
+}
